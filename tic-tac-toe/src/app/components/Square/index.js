c89418ab0c0342c2892f682e15a14ab417f7ac7d
tomiir <rocchitomas@gmail.com>
2018-08-22 13:12:06 -0300

    Divided components in individual folders

---
+++ tic-tac-toe/src/app/components/Square/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
