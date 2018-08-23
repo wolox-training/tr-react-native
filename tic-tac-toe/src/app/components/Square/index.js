8e3fe3d4bb608d5068bec7f46fdb48d37bbe0d30
tomiir <rocchitomas@gmail.com>
2018-08-23 16:13:09 -0300

    Abstracted logic from Square to Board passing down parameters as props.

--- tic-tac-toe/src/app/components/Square/index.js
+++ tic-tac-toe/src/app/components/Square/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  state = {
-    value: null,
-  }
 
   render() {
     return (
-      <button className="square" onClick={()=>{this.setState({value: 'X'})}}>
-        {this.state.value}
+      <button className="square" onClick={()=>{this.props.onClick()}}>
+        {this.props.value}
       </button>
     );
   }
