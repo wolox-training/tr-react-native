da23b5a38e76c77a588547b10151b53ae7cd56aa
tomiir <rocchitomas@gmail.com>
2018-08-23 18:07:27 -0300

    Minor eslint changes

--- tic-tac-toe/src/app/components/Square/index.js
+++ tic-tac-toe/src/app/components/Square/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-function Square(props){
-    return (
-      <button className="square" onClick={()=>{props.onClick()}}>
-        {props.value}
-      </button>
-    );
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        props.onClick();
+      }}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
