6e7da8f96485b9bbb4f348d6aeae0b084cba324c
tomiir <rocchitomas@gmail.com>
2018-08-23 16:16:40 -0300

    Added turn logic.

--- tic-tac-toe/src/app/components/Board/index.js
+++ tic-tac-toe/src/app/components/Board/index.js
@@ -5,12 +5,13 @@ import './index.css';
 class Board extends React.Component {
   state = {
     squares: Array(9).fill(null),
+    xIsNext: true,
   }
-  
+
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    xIsNext: !this.state.xIsNext,
   }
 
   renderSquare(i) {
@@ -23,7 +24,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
