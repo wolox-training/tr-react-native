b797569e5014db1d38df053724452f7b375d570d
tomiir <rocchitomas@gmail.com>
2018-08-30 18:43:16 -0300

    Added action creator for login

--- tic-tac-toe/src/redux/game/actions.js
+++ tic-tac-toe/src/redux/game/actions.js
@@ -12,17 +12,13 @@ export function gameJumpTo(step) {
   };
 }
 
+export const actions = {
+  SQUARE_CLICKED: 'SQUARE_CLICKED',
+  HISTORY_ITEM_SELECTED: 'HISTORY_ITEM_SELECTED'
+}
+
 
 export const actionCreators = {
   gameHandleClick,
-  gameJumpTo,
-  asyncRequest: () => async dispatch => {
-    dispatch({ type: actions.FETCH_USER_DATA });
-    const response = await getUserData();//Ver parte de API
-    if(response.ok) {
-      dispatch({ type: actions.SUBMIT, data: response.data });
-    } else {
-      dispatch({type: actions.REJECT, error: response.error });
-    }
-  }
+  gameJumpTo
 };
---
+++ tic-tac-toe/src/redux/login/actions.js
@@ -0,0 +1,17 @@
+export const actions = {
+  FETCH_USER_DATA: 'FETCH_USER_DATA',
+  SUBMIT: 'SUBMIT',
+  REJECT: 'REJECT'
+};
+
+export const actionCreators = {
+  asyncRequest: () => async dispatch => {
+    dispatch({ type: actions.FETCH_USER_DATA });
+    const response = await getUserData(); // Ver parte de API
+    if(response.ok) {
+      dispatch({ type: actions.SUBMIT, data: response.data });
+    } else {
+      dispatch({type: actions.REJECT, error: response.error });
+    }
+  }
+};
